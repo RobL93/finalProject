aa6d190dc8cc98b5eb3fd3072877ac336b995b30
RobL93 <roberttjlynam@gmail.com>
2025-09-08 22:08:56 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://RobL93.github.io/finalProject/', // full URL with trailing slash
+  base: '/finalProject/', // leading & trailing slash
+  outDir: './docs',       // output into /docs for GitHub Pages
+});
